9cfd64583fb50687cf9708ab891667a5327c0554
Zakria Younas <zakriayounas01@gmail.com>
2024-08-20 18:15:09 +0500

    added html file for home path

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require("cors")
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json())
+app.use(express.static('public'));
 app.use("/", getRoutes);
 connectToDb((err) => {
     if (err) {
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router()
+const path = require("path")
 const { getAllRestaurants, viewRestaurant, addRestaurant, updateRestaurant, deleteRestaurant } = require("../controllers/restaurantController");
 const login = require("../controllers/usersController");
-router.get('/', (req, res) => {
-    res.json('home');
-});
 // private route
 router.route("/restaurants").get(getAllRestaurants)
 router.route("/single-restaurant/:id").get(viewRestaurant)
